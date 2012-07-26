c573d8c00637f1dfc7adb475e1628d7432f660c0
Zachary Alex Stern <Zachary Alex Stern>
2012-07-25 21:26:22 -0400

    Fixed typo. write becomes right.

--- ex6/ex6.rb
+++ ex6/ex6.rb
@@ -15,6 +15,6 @@ joke_evaluation = "Isn't that joke so funny!? #{hilarious}"
 puts joke_evaluation
 
 w = "This is the left side of..."
-e = "a string with a wright side."
+e = "a string with a right side."
 
 puts w + e
