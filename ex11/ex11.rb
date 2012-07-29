602d8614fe6fa79acc45bcf0fafcc5307b9da647
Zachary Alex Stern <zacharyalexstern@gmail.com>
2012-07-29 00:01:28 -0400

    ex11 complete

--- ex11/ex11.rb
+++ ex11/ex11.rb
@@ -6,3 +6,11 @@ print "How much do you weigh? "
 weight = gets.chomp()
 
 puts "So, you're #{age} years old, #{height} tall and #{weight} heavy."
+
+
+print "What's your name? "
+name = gets.chomp()
+puts "Your name is #{name}? That's interesting."
+print "What kind of name is that? "
+nametype = gets.chomp()
+puts "#{nametype}!? That's awesome!"
