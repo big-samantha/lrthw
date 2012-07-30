624306290add4d70b7966ade537509357be1cc72
Zachary Alex Stern <zacharyalexstern@gmail.com>
2012-07-29 21:48:38 -0400

    Completed ex13 ec3.

---
+++ ex13/ex13-ec3.rb
@@ -0,0 +1,7 @@
+computer, os = ARGV
+
+puts "This computer is called #{computer}."
+puts "What are you doing with your computer right now?"
+puts "So you're #{STDIN.gets.chomp} on Ubuntu?"
+puts "Wow, that's interesting!"
+
