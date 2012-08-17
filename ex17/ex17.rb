bd0799257d0e320f95b8656e80ff0df015714f66
Zachary Alex Stern <zacharyalexstern@gmail.com>
2012-08-17 18:30:26 -0400

    Shortened ex17

--- ex17/ex17.rb
+++ ex17/ex17.rb
@@ -7,11 +7,11 @@ puts "Copying from #{from_file} to #{to_file}"
 input = File.open(from_file)
 indata = input.read()
 
-puts "The input file is #{indata.length} bytes long"
+# puts "The input file is #{indata.length} bytes long"
 
-puts "Does the output file exist? #{File.exists? to_file}"
-puts "Ready, hit RETURN to continue, CTRL-C to abort."
-STDIN.gets
+#puts "Does the output file exist? #{File.exists? to_file}"
+#puts "Ready, hit RETURN to continue, CTRL-C to abort."
+#STDIN.gets
 
 output = File.open(to_file, 'w')
 output.write(indata)
