414debf1a6c3501168eb7109ede65cf8ba26b58f
Zachary Alex Stern <zacharyalexstern@gmail.com>
2012-12-13 21:34:16 -0500

    Commented ex24.

--- ex24/ex24.rb
+++ ex24/ex24.rb
@@ -1,6 +1,10 @@
+# print a string
 puts 'Let\'s practice everything.'
+# print another string.
 puts 'You\'d need to know \'bout excapes with \\ that do \n newlines and \t tabs.'
 
+# preformatted multiline string. only exception is that it can use escape chars
+# saved to variable
 poem = <<MULTI_LINE_STRING
 
 \tThe lovely world
@@ -12,13 +16,20 @@ and requires an explanation
 
 MULTI_LINE_STRING
 
+# print out the poem variable which contains a multiline preformatted
+# string
 puts '----------------'
 puts poem
 puts '----------------'
 
+# set variable 5 equal to non-floating number result of 10 - 2 + 3 - 6
 five = 10 - 2 + 3 - 6
+# print results
 puts "This should be five: #{five}"
 
+# create a function called secret_formula
+# declare variable started as part of that formula
+# return as final values, jelly_beans, jars and crates variables.
 def secret_formula(started)
   jelly_beans = started * 500
   jars = jelly_beans / 1000
@@ -29,7 +40,13 @@ end
 start_point = 10000
 beans, jars, crates = secret_formula(start_point)
 
-puts "With a starting point of: #{start_point}"
-puts "We'd have #{beans} beans, #{jars} jars, and #{crates} creates."
+puts "With a starting point of: #{start_point}:"
+puts "We'd have #{beans} beans, #{jars} jars, and #{crates} crates."
 
 start_point = start_point / 10
+
+puts "We can also do that this way:"
+
+# this line passes the three resulting vars from scecret_formula to the format
+# strings
+puts "We'd have %s beans, %s jars, %s crates." % secret_formula(start_point)
