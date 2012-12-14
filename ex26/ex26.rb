26b7b0c15d80ea59d96b2c34b9452a706c8d8de3
Zachary Alex Stern <zs@enternewmedia.com>
2012-12-14 15:32:26 -0500

    fixed one last thing

--- ex26/ex26.rb
+++ ex26/ex26.rb
@@ -75,7 +75,7 @@ beans, jars, crates = secret_formula(start_point)
 puts "With a starting point of: %d" % start_point
 puts "We'd have %d jeans, %d jars, and %d crates." % [beans, jars, crates]
 
-start_point = start_point / 10
+#start_point = start_point / 10
 
 puts "We can also do that this way:"
 puts "We'd have %d beans, %d jars, and %d crates." % secret_formula(start_point)
