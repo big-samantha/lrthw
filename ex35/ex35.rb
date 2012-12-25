959d5ce3243b79194bbb1a2ac393c27a314849cb
Zachary Alex Stern <zacharyalexstern@gmail.com>
2012-12-24 20:04:13 -0500

    Finished ex35 and fixed typos.

--- ex35/ex35.rb
+++ ex35/ex35.rb
@@ -3,13 +3,13 @@ def prompt()
 end
 
 def gold_room()
-  puts "This room is full of gold. How much do you take?"
-  
+  puts "This room is full of gold.  How much do you take?"
+
   prompt; next_move = gets.chomp
   if next_move.include? "0" or next_move.include? "1"
     how_much = next_move.to_i()
   else
-    dead("Man, learn to type a nuber.")
+    dead("Man, learn to type a number.")
   end
 
   if how_much < 50
@@ -20,6 +20,7 @@ def gold_room()
   end
 end
 
+
 def bear_room()
   puts "There is a bear here."
   puts "The bear has a bunch of honey."
@@ -28,7 +29,7 @@ def bear_room()
   bear_moved = false
 
   while true
-      prompt; next_move = gets.chomp
+    prompt; next_move = gets.chomp
 
     if next_move == "take honey"
       dead("The bear looks at you then slaps your face off.")
@@ -36,33 +37,33 @@ def bear_room()
       puts "The bear has moved from the door. You can go through it now."
       bear_moved = true
     elsif next_move == "taunt bear" and bear_moved
-      dead("The bear gets pissed and chews your leg off.")
+      dead("The bear gets pissed off and chews your leg off.")
     elsif next_move == "open door" and bear_moved
-    gold_room()
+      gold_room()
     else
-      puts "I got no idea what that means"
+      puts "I got no idea what that means."
     end
   end
 end
 
-def cthulu_room()
-  puts "Here you see the great evil Cthulu."
+def cthulhu_room()
+  puts "Here you see the great evil Cthulhu."
   puts "He, it, whatever stares at you and you go insane."
   puts "Do you flee for your life or eat your head?"
 
   prompt; next_move = gets.chomp
-  
+
   if next_move.include? "flee"
     start()
   elsif next_move.include? "head"
     dead("Well that was tasty!")
   else
-    cthulu_room()
+    cthulhu_room()
   end
 end
 
 def dead(why)
-  puts "#{why} Good job!"
+  puts "#{why}  Good job!"
   Process.exit(0)
 end
 
@@ -76,12 +77,10 @@ def start()
   if next_move == "left"
     bear_room()
   elsif next_move == "right"
-    cthulu_room()
+    cthulhu_room()
   else
     dead("You stumble around the room until you starve.")
   end
 end
 
 start()
-  end
-end
